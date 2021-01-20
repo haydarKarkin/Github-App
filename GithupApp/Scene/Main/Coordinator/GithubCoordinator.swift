954e0cacf8c3098f10f37e6aa62c76ff68ff0530
Haydar Karkin <haydar.karkin@gmail.com>
2021-01-21 00:33:00 +0300

    Add github module factory class and implement it into module's coordinator

--- GithupApp/Scene/Main/Coordinator/GithubCoordinator.swift
+++ GithupApp/Scene/Main/Coordinator/GithubCoordinator.swift
@@ -8,8 +8,32 @@
 import Foundation
 import UIKit
 
-protocol GithubCoordinatorType {
+protocol GithubCoordinatorType: Coordinator {
+    func goToUserDetailScene()
+    func goToRepoDetailScene()
 }
 
 class GithubCoordinator: GithubCoordinatorType {
+    private let navigationController: UINavigationController
+    private let githubFactory: GithubFactoryType
+    
+    init(navigationController: UINavigationController, githubFactory: GithubFactoryType) {
+        self.navigationController = navigationController
+        self.githubFactory = githubFactory
+    }
+    
+    func start() {
+        let viewController = githubFactory.makeSearchRepoVC()
+        navigationController.pushViewController(viewController, animated: false)
+    }
+    
+    func goToUserDetailScene() {
+        let viewController = githubFactory.makeUserDetailVC()
+        navigationController.pushViewController(viewController, animated: false)
+    }
+    
+    func goToRepoDetailScene() {
+        let viewController = githubFactory.makeRepoDetailVC()
+        navigationController.pushViewController(viewController, animated: false)
+    }
 }
--- GithupApp/Scene/Main/Factory/GithubFactory.swift
+++ GithupApp/Scene/Main/Factory/GithubFactory.swift
@@ -6,9 +6,64 @@
 //
 
 import Foundation
+import UIKit
 
 protocol GithubFactoryType {
+    func makeGithubCoordinator(navigationController: UINavigationController,
+                               githubFactory: GithubFactoryType) -> GithubCoordinatorType
+    func makeGithubService() -> GithubServiceType
+    func makeRepoDetailVM() -> RepoDetailVM
+    func makeSearchRepoVM() -> SearchRepoVM
+    func makeUserDetailVM() -> UserDetailVM
+    func makeRepoDetailVC() -> RepoDetailVC
+    func makeSearchRepoVC() -> SearchRepoVC
+    func makeUserDetailVC() -> UserDetailVC
 }
 
-final class GithubFactory: GithubFactoryType {
+class GithubFactory: GithubFactoryType {
+    
+    let sharedFactory: SharedFactoryType!
+    
+    init(sharedFactory: SharedFactoryType) {
+        self.sharedFactory = sharedFactory
+    }
+    
+    func makeGithubCoordinator(navigationController: UINavigationController,
+                               githubFactory: GithubFactoryType) -> GithubCoordinatorType {
+        return GithubCoordinator(navigationController: navigationController, githubFactory: self)
+    }
+    
+    func makeGithubService() -> GithubServiceType {
+        return GithubService()
+    }
+    
+    func makeRepoDetailVM() -> RepoDetailVM {
+        return RepoDetailVM()
+    }
+    
+    func makeSearchRepoVM() -> SearchRepoVM {
+        return SearchRepoVM()
+    }
+    
+    func makeUserDetailVM() -> UserDetailVM {
+        return UserDetailVM()
+    }
+    
+    func makeRepoDetailVC() -> RepoDetailVC {
+        let viewController = RepoDetailVC.instantiate()
+        viewController.viewModel = makeRepoDetailVM()
+        return viewController
+    }
+    
+    func makeSearchRepoVC() -> SearchRepoVC {
+        let viewController = SearchRepoVC.instantiate()
+        viewController.viewModel = makeSearchRepoVM()
+        return viewController
+    }
+    
+    func makeUserDetailVC() -> UserDetailVC {
+        let viewController = UserDetailVC.instantiate()
+        viewController.viewModel = makeUserDetailVM()
+        return viewController
+    }
 }
