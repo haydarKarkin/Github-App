b148a737e5305d394d15e68d31a24bca9a03d3da
Haydar Karkin <haydar.karkin@gmail.com>
2021-01-20 21:29:17 +0300

    Project init

---
+++ GithupApp/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  GithupApp
+//
+//  Created by Haydar Karkin on 20.01.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
