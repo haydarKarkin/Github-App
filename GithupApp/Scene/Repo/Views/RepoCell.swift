7eaa9180429d5ac1c55afd95a7ff54c90c186829
Haydar Karkin <haydar.karkin@gmail.com>
2021-01-23 20:59:48 +0300

    Add table view cell for showing repo items in search scene

---
+++ GithupApp/Scene/Repo/Views/RepoCell.swift
@@ -0,0 +1,48 @@
+//
+//  RepoCell.swift
+//  GithupApp
+//
+//  Created by Haydar Karkin on 23.01.2021.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell, Reusable {
+    
+    // UI Elements
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var repoNameLabel: UILabel!
+    
+    // Binders
+    var openUserDetailClosure: ((RepoModel) -> Void)?
+    
+    // Variables
+    var model: RepoModel?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleAvatarTap(_:)))
+        avatarImageView.addGestureRecognizer(tap)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configure(with model: RepoModel, openUserDetail: ((RepoModel) -> Void)?) {
+        repoNameLabel.text = model.fullName
+        userNameLabel.text = model.owner?.login
+        
+        openUserDetailClosure = openUserDetail
+    }
+    
+    @objc func handleAvatarTap(_ sender: UITapGestureRecognizer) {
+        if let repo = model {
+            openUserDetailClosure?(repo)
+        }
+    }
+}
